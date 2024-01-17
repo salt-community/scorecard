7926e41a186273b72b072e708d0fd817ade6afab
Finns841594 <fengy142857@gmail.com>
2024-01-17 09:12:07 +0100

    start designing data structure

--- app/component/ContactsBar.tsx
+++ app/component/ContactsBar.tsx
@@ -2,14 +2,14 @@ import Icon from '@/public/icons/Icon';
 import React from 'react';
 
 const developer = {
-  gitHubUrl: 'https://github.com/Finns841594',
+  githubUrl: 'https://github.com/Finns841594',
   linkedinUrl: 'https://www.linkedin.com/in/feng-yang-511361166/',
 };
 
 const ContactsBar = () => {
   return (
     <div className="flex gap-2">
-      <a href={developer.gitHubUrl} target="_blank">
+      <a href={developer.githubUrl} target="_blank">
         <Icon icon="github" className="h-6 w-6 fill-black" />
       </a>
       <a href={developer.linkedinUrl} target="_blank">
---
+++ app/sampleData.ts
@@ -0,0 +1,18 @@
+export const sampleDeveloper = {
+  developerId: '001',
+  name: 'feng yang',
+  githubUrl: 'https://github.com/Finns841594',
+  linkedinUrl: 'https://www.linkedin.com/in/feng-yang-511361166/',
+  selectedProjectUrls: [],
+  educationInfors: '',
+  spokenLanguages: '',
+};
+
+export const sampleGeneratedDeveloperData = {
+  developerId: '001',
+  githubUserName: 'Finns841594',
+  githubProfilePictureUrl: '',
+  allRepos: [],
+  commitsCount: 500,
+  issuesCount: 100,
+};
