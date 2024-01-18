ee6b86f3f6bdc3f8b4ce187355f2d8f7abd7e4b6
Kevin Gida <kevingida@gmail.com>
2024-01-18 15:01:45 +0100

    set test repository

--- server/src/main/java/com/salt/server/test/TestRepository.java
+++ server/src/main/java/com/salt/server/test/TestRepository.java
@@ -1,4 +1,8 @@
 package com.salt.server.test;
 
-public interface TestRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestRepository extends JpaRepository<Test, String> {
 }
