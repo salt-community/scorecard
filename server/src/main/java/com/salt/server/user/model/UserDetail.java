64d6d05cc417671cb47245fc232962e611a3fedc
Kevin Gida <kevingida@gmail.com>
2024-01-19 10:29:12 +0100

    add model for user detail

---
+++ server/src/main/java/com/salt/server/user/model/UserDetail.java
@@ -0,0 +1,20 @@
+package com.salt.server.user.model;
+
+import com.salt.server.github.Github;
+import jakarta.persistence.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+public class UserDetail {
+    @Id
+    @UuidGenerator
+    private UUID id;
+    private String education;
+    @Enumerated(EnumType.STRING)
+    private Bootcamp bootcamp;
+    @OneToOne
+    @JoinColumn(name = "social_id", referencedColumnName = "id")
+    private Social social;
+}
