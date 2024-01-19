81953b97f3a332a4d700b33de907a6e3dc47eb74
Kevin Gida <kevingida@gmail.com>
2024-01-19 10:29:05 +0100

    add model for social

---
+++ server/src/main/java/com/salt/server/user/model/Social.java
@@ -0,0 +1,24 @@
+package com.salt.server.user.model;
+
+import com.salt.server.github.Github;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+public class Social {
+    @Id
+    @UuidGenerator
+    private UUID id;
+    private String linkedInUrl;
+    private String codewarsUrl;
+    @OneToOne
+    @JoinColumn(name = "github_id", referencedColumnName = "id")
+    private Github githubId;
+    @OneToOne
+    private UserDetail userDetail;
+}
