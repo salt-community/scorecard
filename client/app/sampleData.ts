6e8e48f2d2471786bb57eb3ea35dce8bb831b3d8
Finns841594 <fengy142857@gmail.com>
2024-01-19 10:01:32 +0100

    header infos done

--- client/app/sampleData.ts
+++ client/app/sampleData.ts
@@ -1,6 +1,8 @@
-export const sampleDeveloper = {
+export const sampleRegisteredDeveloperData = {
   developerId: '001',
   name: 'feng yang',
+  standoutIntro: 'Experienced in various of client projects',
+  mainProgrammingLanguage: 'javascript',
   githubUrl: 'https://github.com/Finns841594',
   linkedinUrl: 'https://www.linkedin.com/in/feng-yang-511361166/',
   selectedProjectUrls: [],
