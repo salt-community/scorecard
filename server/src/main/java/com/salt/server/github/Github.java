f5c629e9121bf69fcaaf0295fc8f4676bc48f538
Kevin Gida <kevingida@gmail.com>
2024-01-19 13:26:49 +0100

    refactor: delete social field from github

--- server/src/main/java/com/salt/server/github/Github.java
+++ server/src/main/java/com/salt/server/github/Github.java
@@ -1,9 +1,7 @@
 package com.salt.server.github;
 
-import com.salt.server.user.model.Social;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
