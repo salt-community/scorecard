6fb54827ad31c9572e6a951de89fe92aab7aba2b
Finns841594 <fengy142857@gmail.com>
2024-01-24 13:44:17 +0100

    fix deploy error again

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,16 @@
 import ScoreCard from './component/ScoreCard';
 import { sampleGeneratedDeveloperData } from './sampleData';
 
-const fetchUserByAccountId = async (id: string) => {
-  const res = await fetch(
-    'http://localhost:8080/api/account/70694d26-adeb-4ea2-b39c-80e0bd950973'
-  );
-  return res.json();
-};
+// const fetchUserByAccountId = async (id: string) => {
+//   const res = await fetch(
+//     'http://localhost:8080/api/account/70694d26-adeb-4ea2-b39c-80e0bd950973'
+//   );
+//   return res.json();
+// };
 
 export default async function Home() {
-  const developerData = await fetchUserByAccountId('');
-  console.log('developerData: ', developerData);
+  // const developerData = await fetchUserByAccountId('');
+  // console.log('developerData: ', developerData);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12 md:p-24">
