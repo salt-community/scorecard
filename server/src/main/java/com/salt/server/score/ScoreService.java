04347e5f2e8a6abbaea78e5161b96b93dd438eaf
Kevin Gida <kevingida@gmail.com>
2024-01-24 15:11:08 +0100

    feat: add radar graph;

--- server/src/main/java/com/salt/server/score/ScoreService.java
+++ server/src/main/java/com/salt/server/score/ScoreService.java
@@ -30,7 +30,9 @@ public class ScoreService {
 
     public ScoreDto.ScoreListResponse getAllScoreById(UUID id) {
         List<Score> scores = scoreRepository.findAllByAccount_Id(id);
-        return new ScoreDto.ScoreListResponse( scores.stream().map(score -> new ScoreDto.ScoreResponse(score.getId(), score.getAssignment().getName(),score.getScore())).collect(Collectors.toList()));
+        return new ScoreDto.ScoreListResponse(scores.stream()
+                .map(score -> new ScoreDto.ScoreResponse(score.getId(), score.getAssignment().getName(), score.getScore()))
+                .collect(Collectors.toList()));
     }
 
     public ScoreDto.ScoreResponse addScore(UUID id, ScoreDto.ScoreRequest request) {
@@ -43,32 +45,31 @@ public class ScoreService {
         score.setScore(request.score());
         score.setDescription(request.description());
         Score saveScore = scoreRepository.save(score);
-        return new ScoreDto.ScoreResponse(saveScore.getId(),saveScore.getAssignment().getName(), saveScore.getScore());
+        return new ScoreDto.ScoreResponse(saveScore.getId(), saveScore.getAssignment().getName(), saveScore.getScore());
     }
 
     public List<ScoreDto.ScoreResponse> addListOfScores(UUID id, List<ScoreDto.ScoreRequest> requests) {
         List<ScoreDto.ScoreResponse> scoreResponses = new ArrayList<>();
-        for(var score: requests) {
-            scoreResponses.add(addScore(id,score));
+        for (var score : requests) {
+            scoreResponses.add(addScore(id, score));
         }
         return scoreResponses;
     }
 
     public List<AccountDto.RadarGraph> calculateRadarGraph(Account account) {
-
         List<AccountDto.RadarGraph> radarGraph = new ArrayList<>();
-        for(var focus : Focus.values()) {
-            List<Score> allScore = account.getScores();
+        for (var focus : Focus.values()) {
+
             double totalScore = 0;
             double totalPercentage = 0;
-            for(var score: allScore) {
-                double percentage = (double) coverageRepository.findByAssignment_IdAndFocus(score.getAssignment().getId(), focus).getPercentage() /100;
+
+            for (var score : account.getScores()) {
+                double percentage = (double) coverageRepository
+                        .findByAssignment_IdAndFocus(score.getAssignment().getId(), focus).getPercentage() / 100;
                 totalScore += score.getScore() * percentage;
                 totalPercentage += percentage;
             }
-
-            radarGraph.add(new AccountDto.RadarGraph(focus.name(),totalScore/totalPercentage,100));
-            totalScore = 0;
+            radarGraph.add(new AccountDto.RadarGraph(focus.name(), totalScore / totalPercentage, 100));
         }
         return radarGraph;
     }
