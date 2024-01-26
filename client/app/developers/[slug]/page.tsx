dcfdbae9c35c120739bafc4f49f661d532fb0780
JacobLars <jacoblar2001@gmail.com>
2024-01-26 11:13:49 +0100

    fix: fetch issue

--- client/app/developers/[slug]/page.tsx
+++ client/app/developers/[slug]/page.tsx
@@ -4,21 +4,21 @@ import { sampleGeneratedDeveloperData } from '../../sampleData';
 // should apply TanStack Query for fetching
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 const fetchUserByAccountId = async (id: string) => {
-  const res = await fetch(`https://scorecard-l6oa.onrender.com/api/developers/` + id);
+  const res = await fetch(`https://scorecard-l6oa.onrender.com/api/developers/` + id, {
+    cache: "no-cache"
+  });
   return res.json();
 };
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const developerData = await fetchUserByAccountId(params.slug);
-  console.log(params.slug);
+  console.log(developerData);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12 md:p-24">
       <div>
         <ScoreCard
           developerData={
-            developerData.name  // for testing, should removed before formal released
-              ? sampleGeneratedDeveloperData
-              : developerData
+            developerData
           }
         />
       </div>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,22 +1,22 @@
+
 import { UUID } from 'crypto';
 import Link from 'next/link';
 
 // should apply TanStack Query for fetching
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 const fetchAllUsers = async () => {
-  const res = await fetch(`https://scorecard-l6oa.onrender.com/api/developers`);
+  const res = await fetch(`https://scorecard-l6oa.onrender.com/api/developers`, {
+    cache: "no-cache"
+  });
   const data = await res.json();
-  //data.then(d => console.log(d));
+  console.log(res);
   return data;
 };
 
-export const httpGetAllFigures = async () => {
-  const response = await fetch("https://scorecard-l6oa.onrender.com/api/developers");
-  return response.json();
-};
+
 
 type developerInList = {
-  id: UUID;
+  id: string;
   name: string;
   profilePicture: string;
   standoutIntro: string;
@@ -24,19 +24,18 @@ type developerInList = {
 
 export default async function Home() {
   const developersData = await fetchAllUsers();
-  console.log(await httpGetAllFigures());
+  console.log(developersData);
   return (
     <main className="flex min-h-screen flex-col items-center p-12 md:p-24">
       <div>
-        
-        <h1>{developersData.map((x) => (<h2>{x.id}</h2>))}</h1>
-        /* {developersData.map((item: developerInList) => (
+             
+         {developersData.map((item: developerInList) => (
           <li key={item.id}>
             <p>---- Card Component ----</p>
             <Link href={`/developers/${item.id}`}>{item.name}</Link>
             <p>---------------------------</p>
           </li>
-        ))} */
+        ))} 
       </div>
     </main>
   );
