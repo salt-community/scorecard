784ce6e6a2e7e8886e94d19848b42b76340ff750
Kevin Gida <kevingida@gmail.com>
2024-01-29 10:01:53 +0100

    deploy new render

--- server/src/main/java/com/salt/server/seeder/DatabaseSeeder.java
+++ server/src/main/java/com/salt/server/seeder/DatabaseSeeder.java
@@ -36,8 +36,6 @@ public class DatabaseSeeder implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
- assignmentSeeder();
- accountSeeder();
     }
 
     private void assignmentSeeder() {
