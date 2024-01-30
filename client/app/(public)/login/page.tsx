503344f6ad8ace0614afea167e63c4423dc66c2b
JacobLars <jacoblar2001@gmail.com>
2024-01-30 11:14:14 +0100

    add: form for login page

--- client/app/(public)/login/page.tsx
+++ client/app/(public)/login/page.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
+import { Button } from "@material-tailwind/react";
+import { Input, Select, SelectItem } from "@nextui-org/react";
 
 const page = () => {
+  const [input, setInput] = useState({
+    email: "",
+    name: "",
+    role: "core"    
+  });
+
+  const handleOnChange = (event: any) => {
+    const { name, value } = event.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  }
+
+  console.log(input);
   return (
-    <div>page</div>
+    <div>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <Input name='email' type='email' onChange={handleOnChange} placeholder='Email:'></Input>
+        <Input name='name' type='text' onChange={handleOnChange} placeholder='Name:'></Input>
+        <Button
+            className="bg-accent2 hover:bg-accent text-white font-bold py-2 px-4 rounded"
+            placeholder={undefined}
+            type="submit"
+          >
+            Login
+        </Button>
+      </form>
+    </div>
   )
 }
 
