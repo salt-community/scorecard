0e87b7088f8bb556809fb4c1440b7e6f4ef69d86
Kevin Gida <kevingida@gmail.com>
2024-02-02 10:53:09 +0100

    add logout button

--- client/app/components/WebHeader.tsx
+++ client/app/components/WebHeader.tsx
@@ -1,6 +1,16 @@
+"use client";
 import { Link, Navbar, NavbarBrand } from "@nextui-org/react";
+import { useCookies } from "next-client-cookies";
+import { useRouter } from "next/navigation";
 
 const WebHeader = () => {
+  const cookies = useCookies();
+  const router = useRouter();
+
+  const handleLogOut = () => {
+    cookies.remove("salt_role");
+    router.push("/");
+  };
   return (
     <div className="w-full flex justify-center ">
       <Navbar className="flex flex-row justify-center">
@@ -10,8 +20,11 @@ const WebHeader = () => {
           </NavbarBrand>
         </Link>
         <a href="/login">
-          <button className="bg-accent2 hover:bg-accent text-white font-bold py-2 px-4 rounded">
-            Login
+          <button
+            className="bg-accent2 hover:bg-accent text-white font-bold py-2 px-4 rounded"
+            onClick={() => handleLogOut}
+          >
+            Logout
           </button>
         </a>
       </Navbar>
