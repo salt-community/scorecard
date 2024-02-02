4fea1f58abdb7f37d6696008d2d0458f6e5019d2
Kevin Gida <kevingida@gmail.com>
2024-02-02 08:43:26 +0100

    fixed scoreboard slug

--- client/app/(admin)/dashboard/scoreboard/[slug]/page.tsx
+++ client/app/(admin)/dashboard/scoreboard/[slug]/page.tsx
@@ -10,25 +10,27 @@ import { useEffect, useState } from "react";
 export default function Page({ params }: { params: { slug: string } }) {
   const [developer, setDeveloper] = useState<SaltieData>();
   const [assignment, setAssignment] = useState<Assignment[]>();
+  const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
     const developerData = await httpGetSaltieScoreboard(params.slug);
     setDeveloper(developerData);
     const assignment = await httpGetAllAssignment();
     setAssignment(assignment);
+    setLoading(false);
   };
 
   useEffect(() => {
     fetchData();
-    console.log(developer);
   }, []);
 
-  if (!developer) {
+  if (loading == true) {
     return (
       <div>
         <h1>Loading ...</h1>
       </div>
     );
   }
-  return <Scoreboard developer={developer} assignment={assignment!} />;
+
+  return <Scoreboard developer={developer!} assignment={assignment!} />;
 }
