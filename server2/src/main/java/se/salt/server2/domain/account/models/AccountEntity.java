26a3556e3c61fef4dee8bb4ded89a547c8df4fb9
Carl-Fredrik Skönebrant <carl-fredrik.skonebrant@seb.se>
2024-02-07 16:42:42 +0100

    feat: added integration test for account controller

--- server2/src/main/java/se/salt/server2/domain/account/models/AccountEntity.java
+++ server2/src/main/java/se/salt/server2/domain/account/models/AccountEntity.java
@@ -1,8 +1,11 @@
 package se.salt.server2.domain.account.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,9 +18,11 @@ import java.util.UUID;
 @NoArgsConstructor
 @Getter
 @Entity
+@Table(name = "account")
 public class AccountEntity {
     @Id
     @GeneratedValue
     UUID id;
+
     String emailAddress;
 }
