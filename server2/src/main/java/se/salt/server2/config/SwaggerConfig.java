933eccc0a985aba17cfb12392e02aee805a6ed7e
AnneMariel95 <a.mariel.recio@gmail.com>
2024-02-08 13:15:03 +0100

    remove comment

--- server2/src/main/java/se/salt/server2/config/SwaggerConfig.java
+++ server2/src/main/java/se/salt/server2/config/SwaggerConfig.java
@@ -9,7 +9,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-//@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
