5eca4ef0ef135a6d8de872955ba749911c082e37
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-09 15:20:16 +0100

    refactor: correctly addressing payload field for email address

--- client2/app/components/RegisterAccountForm.tsx
+++ client2/app/components/RegisterAccountForm.tsx
@@ -1,32 +1,38 @@
-import { SyntheticEvent, useState } from "react";
+"use client";
+import { useState } from "react";
 
-export default function RegisterAccountForm(){
+export default function RegisterAccountForm() {
+  const [emailAddress, setEmailAddress] = useState("");
+  const handleEmailChange = (e: any) => {
+    e.preventDefault();
+    setEmailAddress(e.target.value);
+  };
 
-    const [email, setEmail] = useState("");
-    const handleEmailChange = (e: any) => {
-        e.preventDefault();
-        setEmail(e.target.value);
-    }
-    
-    const submitEmail = async() => {
-        const payload = JSON.stringify({email})
-        const response = await fetch("http://localhost:8080/api/v2/account", {
-            method: "POST",
-            body: payload
-        })
-        return response.json();
-    }
+  const submitEmail = async () => {
+    const payload = JSON.stringify({ emailAddress });
+    const response = await fetch("http://localhost:8080/api/v2/accounts", {
+      method: "POST",
+      body: payload,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    });
+    return response.json();
+  };
 
-    const submitEmailChange = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        submitEmail();
-    }
+  const submitEmailChange = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    submitEmail();
+  };
 
-    return(<>
-    <form onSubmit={submitEmailChange}>
+  return (
+    <>
+      <form onSubmit={submitEmailChange}>
         <label htmlFor="Email">Email:</label>
         <input onChange={handleEmailChange} type="Email"></input>
         <button type="submit">Submit</button>
-    </form>
-    </>);
+      </form>
+    </>
+  );
 }
