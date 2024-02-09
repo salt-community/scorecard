c5d6cdac451b9cf053a4091801b60bdaf450db82
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-09 11:41:22 +0100

    feat: extract helper functionality to mapper methods in assignment mapper class

--- server2/src/main/java/se/salt/server2/domain/assignment/service/AssignmentService.java
+++ server2/src/main/java/se/salt/server2/domain/assignment/service/AssignmentService.java
@@ -5,59 +5,26 @@ import org.springframework.stereotype.Service;
 import se.salt.server2.domain.assignment.controller.dto.AssignmentRequest;
 import se.salt.server2.domain.assignment.controller.dto.AssignmentResponse;
 import se.salt.server2.domain.assignment.controller.dto.AssignmentResponses;
-import se.salt.server2.domain.assignment.models.AssignmentCategory;
-import se.salt.server2.domain.assignment.models.AssignmentEntity;
+import se.salt.server2.domain.assignment.mapper.AssignmentMapper;
 import se.salt.server2.domain.assignment.repository.AssignmentRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class AssignmentService {
     private final AssignmentRepository assignmentRepository;
+    private final AssignmentMapper assignmentMapper;
 
-    public AssignmentResponse createAssignment(AssignmentRequest assignmentRequest){
-        AssignmentEntity assignmentEntity = AssignmentEntity.builder()
-                .accountId(assignmentRequest.accountId())
-                .title(assignmentRequest.title())
-                .score(assignmentRequest.score())
-                .description(assignmentRequest.description())
-                .category(AssignmentCategory.valueOf(assignmentRequest.category()))
-                .build();
-
-        AssignmentEntity savedAssignmentEntity = assignmentRepository.save(assignmentEntity);
-
-        return AssignmentResponse.builder()
-                .assignmentId(savedAssignmentEntity.getId())
-                .title(savedAssignmentEntity.getTitle())
-                .score(savedAssignmentEntity.getScore())
-                .description(savedAssignmentEntity.getDescription())
-                .category(String.valueOf(savedAssignmentEntity.getCategory()))
-                .build();
+    public AssignmentResponse createAssignment(AssignmentRequest assignmentRequest) {
+        return assignmentMapper.mapToAssignmentResponse(assignmentRepository.save(assignmentMapper.mapToAssignmentEntity(assignmentRequest)));
     }
 
     public AssignmentResponses getAllAssignments() {
-        List<AssignmentResponse> assignmentResponseList = assignmentRepository.findAll().stream().map(a -> AssignmentResponse.builder()
-                .assignmentId(a.getId())
-                .title(a.getTitle())
-                .score(a.getScore())
-                .description(a.getDescription())
-                .category(String.valueOf(a.getCategory()))
-                .build()).toList();
-        return AssignmentResponses.builder()
-                .assignmentResponseList(assignmentResponseList)
-                .build();
+        return assignmentMapper.mapToAssignmentResponses(assignmentRepository.findAll());
     }
 
     public AssignmentResponse getAssignmentById(UUID assignmentId) {
-        AssignmentEntity assignmentEntity = assignmentRepository.findById(assignmentId).orElseThrow();
-        return AssignmentResponse.builder()
-                .assignmentId(assignmentEntity.getId())
-                .title(assignmentEntity.getTitle())
-                .score(assignmentEntity.getScore())
-                .description(assignmentEntity.getDescription())
-                .category(String.valueOf(assignmentEntity.getCategory()))
-                .build();
+        return assignmentMapper.mapToAssignmentResponse(assignmentRepository.findById(assignmentId).orElseThrow());
     }
 }
