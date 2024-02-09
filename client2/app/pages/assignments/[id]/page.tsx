2a6034c2f8bb96d0e02eb305b8c6112d10839836
JacobLars <jacoblar2001@gmail.com>
2024-02-09 16:44:53 +0100

    fix: router issue

--- client2/app/pages/assignments/[id]/page.tsx
+++ client2/app/pages/assignments/[id]/page.tsx
@@ -1,13 +1,10 @@
-import { useRouter } from "next/router";
+"use client";
+import { useRouter } from "next/navigation";
 
-export default function AssignmentPage(){
-
-    const router = useRouter();
-    return (
-        <>
-        <p>
-            {router.query.id}
-        </p>
-        </>
-    )
+export default function AssignmentPage({ params }: { params: { id: string } }) {
+  return (
+    <>
+      <p>{params.id}</p>
+    </>
+  );
 }
