53b0647d68441ebd237e5b94995fa55ee15a72bd
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-09 15:19:19 +0100

    feat: add  component RegisterAccountForm to homepage

--- client2/app/page.tsx
+++ client2/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import RegisterAccountForm from "./components/RegisterAccountForm";
 
 export default function Home() {
   return (
     <main>
       <p>scorecard</p>
+      <RegisterAccountForm />
     </main>
   );
 }
