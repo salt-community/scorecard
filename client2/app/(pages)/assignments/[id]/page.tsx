16432f0b1a78cf042d7da490bbf996c74a44d827
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-12 11:59:54 +0100

    feat: add list of assignments component to fe

--- client2/app/(pages)/assignments/[id]/page.tsx
+++ client2/app/(pages)/assignments/[id]/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { AddAssignmentForm } from "@/app/components/AddAssignmentForm";
+import { ListAssignmentsForAccount } from "@/app/components/ListAssignmentsForAccount";
 
 export default function AssignmentPage({ params }: { params: { id: string } }) {
   return (
     <>
       <p>{params.id}</p>
       <AddAssignmentForm accountId={params.id} />
+ {/*      <ListAssignmentsForAccount accountId={params.id} /> */}
     </>
   );
 }
