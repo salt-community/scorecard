3943c4a0163e272f5ec87631aa781962443e0ed7
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-12 12:00:33 +0100

    feat: basic structure for displaying list of assignments

--- client2/app/components/ListAssignmentsForAccount.tsx
+++ client2/app/components/ListAssignmentsForAccount.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { ReactNode, useState } from "react";
 
-export const ListAssignmentsForAccount = () => {
-  return (
-    <div>ListAssignmentsForAccount</div>
-  )
+type Assignment = {
+  assignmentId: string,
+  title: string,
+  score: number,
+  description: string,
+  category:string
 }
+
+
+export const ListAssignmentsForAccount = ({
+  accountId,
+}: {
+  accountId: string;
+}): ReactNode => {
+  const [assignments, setAssignments] = useState<Assignment[]>([]);
+  const listOfAssignments = async () => {
+    const response = await fetch(`http://localhost:8080/api/v2/assignments?accountId=${accountId}`, {
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json;
+  };
+
+  return <div>ListAssignmentsForAccount</div>;
+};
