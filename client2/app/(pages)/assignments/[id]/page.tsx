99b3be17812322bb4ae8faf4d59da48be2a045c8
AnneMariel95 <a.mariel.recio@gmail.com>
2024-02-12 13:18:27 +0100

    render ListAssignmentsForAccount page

--- client2/app/(pages)/assignments/[id]/page.tsx
+++ client2/app/(pages)/assignments/[id]/page.tsx
@@ -3,11 +3,12 @@ import { AddAssignmentForm } from "@/app/components/AddAssignmentForm";
 import { ListAssignmentsForAccount } from "@/app/components/ListAssignmentsForAccount";
 
 export default function AssignmentPage({ params }: { params: { id: string } }) {
+  console.log(params)
   return (
     <>
       <p>{params.id}</p>
       <AddAssignmentForm accountId={params.id} />
- {/*      <ListAssignmentsForAccount accountId={params.id} /> */}
+      <ListAssignmentsForAccount accountId={params.id} />
     </>
   );
 }
