a780074456b9b4c63d59e34e61f35e10f3e6ce4c
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-13 09:45:50 +0100

    refactor: create assignmentforminfo type and consequentaly altered functions implemented

--- client2/app/components/AddAssignmentForm.tsx
+++ client2/app/components/AddAssignmentForm.tsx
@@ -1,41 +1,39 @@
-"use client";
 import React, { ReactNode, useState } from "react";
 
-export const AddAssignmentForm = ({ accountId }: { accountId: string }):ReactNode => {
-  const [title, setTitle] = useState("");
-  const [score, setScore] = useState<number>(0);
-  const [description, setDescription] = useState("");
-  const [category, setCategory] = useState("");
+type AssignmentFormInfo = {
+  title: string;
+  score: number;
+  description: string;
+  category: string;
+};
 
-  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  e.preventDefault();
-  setTitle(e.target.value)
-  }
+export const AddAssignmentForm = ({
+  accountId,
+}: {
+  accountId: string;
+}): ReactNode => {
+  const [assignment, setAssignment] = useState<AssignmentFormInfo>({
+    title: "",
+    score: 0,
+    description: "",
+    category: "",
+  });
 
-    const handleScoreChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setScore(Number(e.target.value))
-    }
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+    setAssignment({
+      ...assignment,
+      [name]: name === "score" ? Number(value) : value,
+    });
+  };
 
-    const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      e.preventDefault();
-      setDescription(e.target.value)
-      }
-
-      const handleSelectCategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        e.preventDefault();
-        setCategory(e.target.value)
-        console.log(category)
-        }
-
-  const submitAssignment = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const submitAssignment = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     const payload = JSON.stringify({
       accountId,
-      title,
-      score,
-      description,
-      category,
+      ...assignment,
     });
     const response = await fetch("http://localhost:8080/api/v2/assignments", {
       method: "POST",
@@ -45,24 +43,44 @@ export const AddAssignmentForm = ({ accountId }: { accountId: string }):ReactNod
         "Content-Type": "application/json",
       },
     });
-    const data = await response.json();
-    console.log(data)
+    const data = response.json();
   };
 
-  return <>
-  <form onSubmit={submitAssignment}>
-    <label htmlFor="title">Title:</label>
-    <input type="text" id="title" value={title} onChange={handleTitleChange}></input>
-    <label htmlFor="score">Score:</label>
-    <input type="number" id="score" value={score} onChange={handleScoreChange}></input>
-    <label htmlFor="description">Description:</label>
-    <input type="text" id="description" value={description} onChange={handleDescriptionChange}></input>
-    <label htmlFor="category">Category:</label>
-    <select onChange={handleSelectCategory}>
-      <option value={"Backend"}>Backend</option>
-      <option value={"Frontend"}>Frontend</option>
-    </select>
-    <button type="submit">Submit</button>
-  </form>
-  </>
+  return (
+    <>
+      <form onSubmit={submitAssignment}>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={assignment.title}
+          onChange={handleInputChange}
+        ></input>
+        <label htmlFor="score">Score:</label>
+        <input
+          type="number"
+          id="score"
+          name="score"
+          value={assignment.score}
+          onChange={handleInputChange}
+        ></input>
+        <label htmlFor="description">Description:</label>
+        <input
+          type="text"
+          id="description"
+          name="description"
+          value={assignment.description}
+          onChange={handleInputChange}
+        ></input>
+        <label htmlFor="category">Category:</label>
+        <select id="category" name="category" onChange={handleInputChange}>
+          <option>Select...</option>
+          <option value={"Backend"}>Backend</option>
+          <option value={"Frontend"}>Frontend</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 };
