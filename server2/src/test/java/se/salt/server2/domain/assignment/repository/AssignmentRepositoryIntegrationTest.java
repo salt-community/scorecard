e9478906440863c7234d4708c458a57de8a07542
Carl-Fredrik Skönebrant <carl-fredrik.skonebrant@seb.se>
2024-02-13 22:03:43 +0100

    feature: added testcontainers to spin up postgres instance for tests, setup test application.yml and fix test

--- server2/src/test/java/se/salt/server2/domain/assignment/repository/AssignmentRepositoryIntegrationTest.java
+++ server2/src/test/java/se/salt/server2/domain/assignment/repository/AssignmentRepositoryIntegrationTest.java
@@ -1,12 +1,10 @@
 package se.salt.server2.domain.assignment.repository;
 
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import se.salt.server2.domain.assignment.models.AssignmentCategory;
 import se.salt.server2.domain.assignment.models.AssignmentEntity;
 
@@ -15,31 +13,24 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@ExtendWith(SpringExtension.class)
-public class AssignmentRepositoryIntegrationTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class AssignmentRepositoryIntegrationTest {
+
     @Autowired
     AssignmentRepository assignmentRepository;
-    @Autowired
-    private TestEntityManager entityManager;
 
-    @BeforeEach
     @Test
-    public void whenFindById_thenReturnAssignment() {
-        AssignmentEntity newAssignment = new AssignmentEntity(
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                "Weekend test 1",
-                86,
-                "Well done!",
-                AssignmentCategory.BACKEND
-        );
+    @SneakyThrows
+    void shouldFindByIdAndReturnAssignment() {
 
-        AssignmentEntity foundAssignment = assignmentRepository.save(newAssignment);
+        var assignment = new AssignmentEntity(
+                UUID.randomUUID(), UUID.randomUUID(), "Weekend test 1", 86, "Well done!", AssignmentCategory.BACKEND);
 
-        assertThat(entityManager.find(AssignmentEntity.class, foundAssignment.getId())).isEqualTo(newAssignment);
+        var assignmentRes = assignmentRepository.save(assignment);
 
-
+        assertThat(assignmentRepository.findById(assignment.getId()).get().getId())
+                .isEqualTo(assignmentRes.getId());
+        assertThat(assignmentRepository.findById(assignment.getId()).get().getDescription())
+                .isEqualTo(assignmentRes.getDescription());
     }
-
-
 }
