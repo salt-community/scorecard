9083020d0dc70c602c4789e12e6c85ec35270369
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-13 09:45:00 +0100

    refactor: remove console.log of the id params

--- client2/app/(pages)/assignments/[id]/page.tsx
+++ client2/app/(pages)/assignments/[id]/page.tsx
@@ -3,10 +3,9 @@ import { AddAssignmentForm } from "@/app/components/AddAssignmentForm";
 import { ListAssignmentsForAccount } from "@/app/components/ListAssignmentsForAccount";
 
 export default function AssignmentPage({ params }: { params: { id: string } }) {
-  console.log(params)
   return (
     <>
-      <p>{params.id}</p>
+      <h1>Hello {params.id}</h1>
       <AddAssignmentForm accountId={params.id} />
       <ListAssignmentsForAccount accountId={params.id} />
     </>
