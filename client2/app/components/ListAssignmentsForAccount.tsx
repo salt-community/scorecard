ea34d29106fa8ffcaa624a16b0fbb3986206be6a
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-14 14:26:50 +0100

    refactor: added previously created design

--- client2/app/components/ListAssignmentsForAccount.tsx
+++ client2/app/components/ListAssignmentsForAccount.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Button, Card } from "@material-tailwind/react";
+
 type Assignment = {
   assignmentId: string;
   title: string;
@@ -30,16 +32,47 @@ export const ListAssignmentsForAccount = async ({
   const filtered = listOfAssignments;
 
   return (
-    <div>
-      <div>List of Assignments For Account</div>
-      {filtered.map((a) => (
-        <div key={a.assignmentId} className="p-2 mb-2 border rounded-sm">
-          <p>{a.category}</p>
-          <p>{a.title}</p>
-          <p>{a.score}</p>
-          <p>{a.description}</p>
+    <>
+      <Card className="p-4 shadow-none" placeholder={undefined}>
+        <div className="overflow-y-auto">
+          <table className="w-full text-sm text-left text-black">
+            <thead className="border text-xs text-black uppercase bg-slate-300">
+              <tr className=" text-text text-center">
+                <th scope="col" className="border px-6 py-3 w-2/5">
+                  Category
+                </th>
+                <th scope="col" className="border px-6 py-3 w-2/5">
+                  Title
+                </th>
+                <th scope="col" className="border px-6 py-3 w-2/5">
+                  Score
+                </th>
+                <th scope="col" className="border px-6 py-3 w-2/5">
+                  Description
+                </th>
+                <th scope="col" className="border px-6 py-3 ">
+                  Edit
+                </th>
+                <th scope="col" className="border px-6 py-3 ">
+                  Delete
+                </th>
+              </tr>
+
+              {filtered.map((a) => (
+                <tr
+                  key={a.assignmentId}
+                  className="even:bg-white odd:bg-gray-100"
+                >
+                  <td className="border px-6 py-4">{a.category}</td>
+                  <td className="border px-6 py-4">{a.title}</td>
+                  <td className="border px-6 py-4">{a.score}</td>
+                  <td className="border px-6 py-4">{a.description}</td>
+                </tr>
+              ))}
+            </thead>
+          </table>
         </div>
-      ))}
-    </div>
+      </Card>
+    </>
   );
 };
