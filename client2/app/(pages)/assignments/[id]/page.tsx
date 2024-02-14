f45287d9dc51ece5bc035d6bbe5f10cb967b5a25
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-14 15:41:37 +0100

    refactor: add function for post method to assignment component

--- client2/app/(pages)/assignments/[id]/page.tsx
+++ client2/app/(pages)/assignments/[id]/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import { AddAssignmentForm } from "@/app/components/AddAssignmentForm";
 import { ListAssignmentsForAccount } from "@/app/components/ListAssignmentsForAccount";
+import { postAssignment } from "@/server";
 
 export default function AssignmentPage({ params }: { params: { id: string } }) {
   return (
     <>
-      <AddAssignmentForm accountId={params.id} />
+      <AddAssignmentForm
+        accountId={params.id}
+        postAssignment={postAssignment}
+      />
       <ListAssignmentsForAccount accountId={params.id} />
     </>
   );
