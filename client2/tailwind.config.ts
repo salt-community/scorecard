bf6a67496eba135ed5b1857d36df9a1c09d1d313
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-14 13:36:34 +0100

    feat: add previous tailwind configuration

--- client2/tailwind.config.ts
+++ client2/tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,8 +15,18 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        accent: "#FF7961",
+        accent2: "#F35C7E",
+        grading1: "#e20c0c",
+        grading2: "#f2831f",
+        grading3: "#fbdd10",
+        grading4: "#85ef08",
+        grading5: "#1bff00",
+      },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
