b98779b6440c9f8c4b43058d3018b588d75f2c76
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-15 14:48:43 +0100

    feat: add developers

---
+++ client2/app/components/AddDeveloperForm.tsx
@@ -0,0 +1,125 @@
+import React, { ReactNode, useState } from "react";
+
+import {
+  Card,
+  CardHeader,
+  Input,
+  Select,
+  SelectItem,
+  Textarea,
+} from "@nextui-org/react";
+import { Button } from "@material-tailwind/react";
+import {
+  DeveloperFormInfo,
+  PostDeveloperFunction,
+  postDeveloper,
+} from "@/server";
+
+type SubmitAssignmentFunction = (developer: DeveloperFormInfo) => Promise<void>;
+
+export const AddDeveloperForm = ({
+  postDeveloper,
+}: {
+  postDeveloper: PostDeveloperFunction;
+}): ReactNode => {
+  const [developer, setDeveloper] = useState<DeveloperFormInfo>({
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    bootcampCourse: "",
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+    setDeveloper({
+      ...developer,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await postDeveloper({ developer });
+  };
+
+  return (
+    <div className="h-full">
+      <Card className="flex mx-10 my-5 px-10">
+        <CardHeader>
+          <h4 className="font-bold text-large">Add new Developer</h4>
+        </CardHeader>
+        <form
+          id="form"
+          onSubmit={handleSubmit}
+          className="flex flex-col h-full justify-start gap-4"
+        >
+          <div className=" w-full p-4 flex flex-row gap-4">
+            <Input
+              type="text"
+              id="firstName"
+              name="firstName"
+              label="First Name :"
+              labelPlacement="outside-left"
+              placeholder="Enter title"
+              value={developer.firstName}
+              onChange={handleInputChange}
+              className="w-72"
+            />
+            <Input
+              type="text"
+              id="lastName"
+              name="lastName"
+              label="Last Name :"
+              labelPlacement="outside-left"
+              placeholder="Enter score"
+              value={developer.lastName}
+              onChange={handleInputChange}
+              className="w-72"
+            />
+            <Input
+              type="text"
+              id="emailAddress"
+              name="emailAddress"
+              label="Email :"
+              labelPlacement="outside-left"
+              placeholder="Enter score"
+              value={developer.emailAddress}
+              onChange={handleInputChange}
+              className="w-72"
+            />
+          </div>
+          <Select
+            id="bootcampCourse"
+            name="bootcampCourse"
+            label="Bootcamp :"
+            labelPlacement="outside"
+            placeholder="Please choose a bootcamp..."
+            className="w-72"
+            onChange={handleInputChange}
+          >
+            <SelectItem key="Javascript" value={"Javascript"}>
+              Javascript
+            </SelectItem>
+            <SelectItem key="Java" value={"Java"}>
+              Java
+            </SelectItem>
+            <SelectItem key=".Net" value={".Net"}>
+              .Net
+            </SelectItem>
+          </Select>
+          <div className="flex flex-row justify-center">
+            <Button
+              className="bg-accent2 hover:bg-accent text-white font-bold py-2 px-4 rounded mb-5"
+              placeholder={undefined}
+              type="submit"
+            >
+              ADD
+            </Button>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
+};
