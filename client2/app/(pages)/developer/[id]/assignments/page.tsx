d3d505b3965f51b27dab7a182ac3a04f375b544b
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-02-16 14:57:47 +0100

    refactor: page add assignments

--- client2/app/(pages)/developer/[id]/assignments/page.tsx
+++ client2/app/(pages)/developer/[id]/assignments/page.tsx
@@ -1,17 +1,18 @@
-"use client"
-import { ListAssignmentsForAccount } from '@/app/components/ListAssignmentsForAccount';
-import { postAssignment } from '@/server';
-import React from 'react'
+"use client";
+import { AddAssignmentForm } from "@/app/components/AddAssignmentForm";
+import { ListAssignmentsForAccount } from "@/app/components/ListAssignmentsForAccount";
+import { postAssignment } from "@/server";
+import React from "react";
 
 const AddAssignments = ({ params }: { params: { id: string } }) => {
-  const id = params.id as string
-  
+  const id = params.id as string;
+
   return (
     <>
-   {/* <AddAssignments /> */}
-    <ListAssignmentsForAccount accountId={params.id}/>
+      <AddAssignmentForm developerId={id} postAssignment={postAssignment} />
+      <ListAssignmentsForAccount developerId={params.id} />
     </>
-  )
-}
+  );
+};
 
 export default AddAssignments;
