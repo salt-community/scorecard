1b6a096c455075d5c95edcee32bfd1c69c4f806b
AnneMariel95 <a.mariel.recio@gmail.com>
2024-02-26 14:33:56 +0100

    display average FE and BE scores

--- client2/features/scorecard/components/scorecardComponents/ScoreCardBody.tsx
+++ client2/features/scorecard/components/scorecardComponents/ScoreCardBody.tsx
@@ -1,26 +1,23 @@
 "use client";
 import { Divider } from "@nextui-org/react";
-//import SaltScore from "./SaltScore";
 import { Developer } from "@/server";
-//import Achievements from "./Achievements";
-//import Projects from "./Projects";
-
 
 interface ScoreCardBodyProps {
   developer: Developer;
 }
 
 const ScoreCardBody = ({ developer }: ScoreCardBodyProps) => {
-
   return (
     <div className="flex flex-col gap-4">
-   <h4>Developer Information</h4>
+      <h4>Developer Information</h4>
       <Divider />
-     {/*  <SaltScore
+      {/*  <SaltScore
         scores={scores}
         radarGraphicData={radarGraph}
         averages={averages}
       /> */}
+      <p>Average Backend Score: {developer.averageBackendScore}</p>
+      <p>Average Frontend Score: {developer.averageFrontendScore}</p>
     </div>
   );
 };
