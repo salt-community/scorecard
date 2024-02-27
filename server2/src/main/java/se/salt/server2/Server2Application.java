cf47e3b5af92e5a7de76de5ef814b49ed7bc0fa3
AnneMariel95 <a.mariel.recio@gmail.com>
2024-02-27 10:29:36 +0100

    add annotation to allow scan of config properties

--- server2/src/main/java/se/salt/server2/Server2Application.java
+++ server2/src/main/java/se/salt/server2/Server2Application.java
@@ -2,7 +2,9 @@ package se.salt.server2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class Server2Application {
 
