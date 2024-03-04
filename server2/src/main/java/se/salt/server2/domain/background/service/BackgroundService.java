6c206ac4506865964dd0af7f42b7f8df64d2ff5a
Marisa Pinheiro <marisa.nbsp@gmail.com>
2024-03-04 15:30:15 +0100

    feat: add service class

---
+++ server2/src/main/java/se/salt/server2/domain/background/service/BackgroundService.java
@@ -0,0 +1,10 @@
+package se.salt.server2.domain.background.service;
+
+import se.salt.server2.domain.background.repository.BackgroundRepository;
+
+public class BackgroundService {
+
+    private final BackgroundRepository backgroundRepository;
+
+
+}
