4ab67746cb02c59f8160dd68e64496c07059c2bd
AnneMariel95 <a.mariel.recio@gmail.com>
2024-03-07 13:22:12 +0100

    add method to fetch all accounts with associated background entities

--- server2/src/main/java/se/salt/server2/domain/account/repository/AccountRepository.java
+++ server2/src/main/java/se/salt/server2/domain/account/repository/AccountRepository.java
@@ -1,11 +1,15 @@
 package se.salt.server2.domain.account.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import se.salt.server2.domain.account.models.AccountEntity;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<AccountEntity, UUID> {
+    @Query("SELECT a FROM AccountEntity a JOIN FETCH a.background")
+    List<AccountEntity> findAllWithBackgroundEntity();
 }
