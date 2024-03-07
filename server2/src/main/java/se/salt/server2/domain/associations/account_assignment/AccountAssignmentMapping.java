45e87625f8da19d301bd3a8599494c6a79afbdaa
AnneMariel95 <a.mariel.recio@gmail.com>
2024-03-07 13:33:16 +0100

    create mapping for account and assignment

---
+++ server2/src/main/java/se/salt/server2/domain/associations/account_assignment/AccountAssignmentMapping.java
@@ -0,0 +1,36 @@
+package se.salt.server2.domain.associations.account_assignment;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+import se.salt.server2.domain.account.models.AccountEntity;
+import se.salt.server2.domain.assignment.models.AssignmentEntity;
+
+import java.util.UUID;
+
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "account_assignment")
+public class AccountAssignmentMapping {
+    // This is optional but could be useful in the future
+    @Id
+    @UuidGenerator
+    @Column
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    private AccountEntity account;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "assignment_id")
+    private AssignmentEntity assignment;
+
+    private int score;
+}
