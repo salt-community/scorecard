17789b0215f42175b7ba1e11d5d673f742b04d7e
AnneMariel95 <a.mariel.recio@gmail.com>
2024-03-07 13:31:46 +0100

    update helper methods

--- server2/src/main/java/se/salt/server2/domain/assignment/mapper/AssignmentMapper.java
+++ server2/src/main/java/se/salt/server2/domain/assignment/mapper/AssignmentMapper.java
@@ -1,33 +1,19 @@
 package se.salt.server2.domain.assignment.mapper;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import se.salt.server2.domain.assignment.controller.dto.AssignmentRequest;
 import se.salt.server2.domain.assignment.controller.dto.AssignmentResponse;
 import se.salt.server2.domain.assignment.controller.dto.AssignmentResponses;
 import se.salt.server2.domain.assignment.models.AssignmentCategory;
 import se.salt.server2.domain.assignment.models.AssignmentEntity;
-import se.salt.server2.domain.developer.models.DeveloperEntity;
-import se.salt.server2.domain.developer.repository.DeveloperRepository;
 
 import java.util.List;
 
 @Component
 public class AssignmentMapper {
-
-
-
-
-
-    public AssignmentEntity mapToAssignmentEntity(AssignmentRequest assignmentRequest, DeveloperEntity developer) {
-
+    public AssignmentEntity mapToAssignmentEntity(AssignmentRequest assignmentRequest) {
         return AssignmentEntity.builder()
-                .developer(developer)
                 .title(assignmentRequest.title())
-                .score(assignmentRequest.score())
                 .description(assignmentRequest.description())
                 .category(AssignmentCategory.fromString(assignmentRequest.category()))
                 .build();
@@ -36,9 +22,7 @@ public class AssignmentMapper {
     public AssignmentResponse mapToAssignmentResponse(AssignmentEntity assignmentEntity) {
         return AssignmentResponse.builder()
                 .assignmentId(assignmentEntity.getId())
-                .developerId(assignmentEntity.getDeveloper().getId())
                 .title(assignmentEntity.getTitle())
-                .score(assignmentEntity.getScore())
                 .description(assignmentEntity.getDescription())
                 .category(String.valueOf(assignmentEntity.getCategory()))
                 .build();
@@ -52,6 +36,5 @@ public class AssignmentMapper {
         return AssignmentResponses.builder()
                 .assignmentResponseList(assignmentResponseList)
                 .build();
-
     }
 }
