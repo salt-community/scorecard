079339a0a9df67604c7cd0e42a3587cfdaebabee
AnneMariel95 <a.mariel.recio@gmail.com>
2024-03-22 10:58:58 +0100

    add query to fetch all accounts excluding developers based on account role mapping

--- server2/src/main/java/se/salt/server2/domain/account/repository/AccountRepository.java
+++ server2/src/main/java/se/salt/server2/domain/account/repository/AccountRepository.java
@@ -12,4 +12,7 @@ import java.util.UUID;
 public interface AccountRepository extends JpaRepository<AccountEntity, UUID> {
     @Query("SELECT a FROM AccountEntity a JOIN FETCH a.background")
     List<AccountEntity> findAllWithBackgroundEntity();
+
+    @Query("SELECT a FROM AccountEntity a WHERE a.id NOT IN (SELECT arm.account.id FROM AccountRoleMapping arm WHERE arm.role.name = 'developer')")
+    List<AccountEntity> findAllNonDevelopers();
 }
